6b3c930e942a2bd0e59f532f4c17ea0c0c4fdd4f
ramaz94 <ramaz635@gmail.com>
2021-03-01 21:44:09 +0200

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
+  $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+    });
 
+  $('.product-slider__inner').slick({
+      dots: true,
+      slidesToShow: 4,
+      arrows: false,
+      slidesToScroll: 4,
+  });
+    
 
+  var mixer = mixitup('.products__inner-box');
 
     
 });
